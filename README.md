0ce85d0e87edd2c12b2a09effe46d5c5dcde8d7d
David Tao <ruoyutao@gmail.com>
2017-01-27 13:29:52 -0500

    add own test images

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # handwriting-recognition
 McGill AI Society presents: Recognizing handwriting using Machine Learning
+
+tutorial based on:
+https://www.tensorflow.org/versions/r0.10/tutorials/mnist/beginners/
+https://niektemme.com/2016/02/21/tensorflow-handwriting/
+
--- mnist_softmax.py
+++ mnist_softmax.py
@@ -2,6 +2,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
+
+
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
@@ -9,18 +11,26 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-#ask oli about the second parameter in tf.reduce_sum
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 
+# for persisting model
+saver = tf.train.Saver()
+
 sess = tf.Session()
+
 sess.run(init)
 
 for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  
+# saving the model
+save_path = saver.save(sess, "./model.ckpt")
+print ("Model saved in file: ", save_path)
+
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 
